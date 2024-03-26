26f35a8b2cd0a79bc75a7ae6f5c64eb29e576490
AndreaAleAM <159818760+AndreaAleAM@users.noreply.github.com>
2024-03-26 12:23:18 -0600

    Explorador de valuación y precios

--- public/Admin/apps-calendar.php
+++ public/Admin/apps-calendar.php
@@ -2,186 +2,103 @@
 <?php include 'layouts/head-main.php'; ?>
 
 <head>
-    <title>Calendar | Minia - Admin & Dashboard Template</title>
+    <title>Menú | DatAlpine</title>
     <?php include 'layouts/head.php'; ?>
 
 
-        <?php include 'layouts/head-style.php'; ?>
+    <?php include 'layouts/head-style.php'; ?>
 </head>
 
 <?php include 'layouts/body.php'; ?>
 
-        <!-- Begin page -->
-        <div id="layout-wrapper">
+<!-- Begin page -->
+<div id="layout-wrapper">
 
-        <?php include 'layouts/menu.php'; ?>
+    <?php include 'layouts/menu.php'; ?>
 
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
 
-                <div class="page-content">
-                    <div class="container-fluid">
+        <div class="page-content">
+            <div class="container-fluid">
 
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                    <h4 class="mb-sm-0 font-size-18">Calendar</h4>
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                            <h4 class="mb-sm-0 font-size-18">Explorador de valuación y precios</h4>
 
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Apps</a></li>
-                                            <li class="breadcrumb-item active">Calendar</li>
-                                        </ol>
-                                    </div>
-
-                                </div>
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Menú</a></li>
+                                    <li class="breadcrumb-item active">Explorador de valuación y precios</li>
+                                </ol>
                             </div>
+
                         </div>
-                        <!-- end page title -->
-
-                        <div class="row">
-                            <div class="col-12">
-                               
-                                <div class="row">
-                                    <div class="col-xl-3 col-lg-4">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div class="d-grid">
-                                                    <button class="btn font-16 btn-primary" id="btn-new-event"><i class="mdi mdi-plus-circle-outline"></i> Create
-                                                        New Event</button>
-                                                </div>
-                                                
-                                                <div id="external-events" class="mt-2">
-                                                    <br>
-                                                    <p class="text-muted">Drag and drop your event or click in the calendar</p>
-                                                    <div class="external-event fc-event text-success bg-success-subtle" data-class="bg-success">
-                                                        <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>New Event Planning
-                                                    </div>
-                                                    <div class="external-event fc-event text-info bg-info-subtle" data-class="bg-info">
-                                                        <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Meeting
-                                                    </div>
-                                                    <div class="external-event fc-event text-warning bg-warning-subtle" data-class="bg-warning">
-                                                        <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Generating Reports
-                                                    </div>
-                                                    <div class="external-event fc-event text-danger bg-danger-subtle" data-class="bg-danger">
-                                                        <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Create New theme
-                                                    </div>
-                                                    <div class="external-event fc-event text-dark bg-dark-subtle" data-class="bg-dark">
-                                                        <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Team Meeting
-                                                    </div>
-                                                </div>
-
-                                                <div class="row justify-content-center mt-5">
-                                                    <div class="col-lg-12 col-sm-6">
-                                                        <img src="assets/images/undraw-calendar.svg" alt="" class="img-fluid d-block">
-                                                    </div>
-                                                </div>
-                                            
-                                            </div>
-                                        </div>
-                                    </div> <!-- end col-->
-
-                                    <div class="col-xl-9 col-lg-8">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div id="calendar"></div>
-                                            </div>
-                                        </div>
-                                    </div> <!-- end col -->
-
-                                </div> 
-
-                                <div style='clear:both'></div>
-
-
-                                <!-- Add New Event MODAL -->
-                                <div class="modal fade" id="event-modal" tabindex="-1">
-                                    <div class="modal-dialog modal-dialog-centered">
-                                        <div class="modal-content">
-                                            <div class="modal-header py-3 px-4 border-bottom-0">
-                                                <h5 class="modal-title" id="modal-title">Event</h5>
-
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
-
-                                            </div>
-                                            <div class="modal-body p-4">
-                                                <form class="needs-validation" name="event-form" id="form-event" novalidate>
-                                                <input type="hidden" name="eventid" id="eventid">
-                                                    <div class="row">
-                                                        <div class="col-12">
-                                                            <div class="mb-3">
-                                                                <label class="form-label">Event Name</label>
-                                                                <input class="form-control" placeholder="Insert Event Name"
-                                                                    type="text" name="title" id="event-title" required value="" />
-                                                                <div class="invalid-feedback">Please provide a valid event name</div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-12">
-                                                            <div class="mb-3">
-                                                                <label class="form-label">Category</label>
-                                                                <select class="form-control form-select" name="category" id="event-category">
-                                                                    <option  selected> --Select-- </option>
-                                                                    <option value="bg-danger">Danger</option>
-                                                                    <option value="bg-success">Success</option>
-                                                                    <option value="bg-primary">Primary</option>
-                                                                    <option value="bg-info">Info</option>
-                                                                    <option value="bg-dark">Dark</option>
-                                                                    <option value="bg-warning">Warning</option>
-                                                                </select>
-                                                                <div class="invalid-feedback">Please select a valid event category</div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row mt-2">
-                                                        <div class="col-6">
-                                                            <button type="button" class="btn btn-danger" id="btn-delete-event">Delete</button>
-                                                        </div>
-                                                        <div class="col-6 d-flex text-end">
-                                                            <button type="button" class="btn btn-light me-1" data-bs-dismiss="modal">Close</button>
-                                                            <button type="submit" class="btn btn-success" id="btn-save-event">Save</button>
-                                                            <button type="submit" class="btn btn-success" id="edit-event-btn">Update</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div> <!-- end modal-content-->
-                                    </div> <!-- end modal dialog-->
-                                </div>
-                                <!-- end modal-->
-
-                            </div>
-                        </div>
-                        
-                    </div> <!-- container-fluid -->
+                    </div>
                 </div>
-                <!-- End Page-content -->
-                
-                <?php include 'layouts/footer.php'; ?>
+                <!-- end page title -->
+
+                <!-- Explorador de valuación y precios -->
+                <div class="row align-items-center"
+                    style="border: 1px solid #86ADFF; border-radius: 5px;  padding: 12px; margin-bottom: 20px;">
+                    <style>
+                        /* Estilos CSS para el título */
+                        .titulo {
+                            text-align: center;
+                            /* Centrar el texto */
+                            color: #333;
+                            /* Color llamativo (en este caso, naranja) */
+                            font-size: 24px;
+                            /* Tamaño del texto */
+                            font-weight: bold;
+                            /* Negrita */
+                            font-family: Arial, sans-serif;
+                            /* Fuente del texto */
+                        }
+                    </style>
+                    <div class="titulo">
+                        Explorador de valuación y precios
+                    </div>
+                    <div style="border-top: 1px solid #86ADFF; padding-top: 10px;">
+                        <p style="font-size: 16px; line-height: 1.5; color: #666;">Estamos trabajando diligentemente
+                            para brindarte una experiencia única y enriquecedora. Mantente al tanto, ¡pronto estará
+                            lista para ti!
+                        </p>
+                    </div>
+
+
+                </div> <!-- container-fluid -->
             </div>
-            <!-- end main content-->
+            <!-- End Page-content -->
 
+            <?php include 'layouts/footer.php'; ?>
         </div>
-        <!-- END layout-wrapper -->
+        <!-- end main content-->
 
-        
-        <!-- Right Sidebar -->
-            <?php include 'layouts/right-sidebar.php'; ?>
-        <!-- /Right-bar -->
+    </div>
+    <!-- END layout-wrapper -->
 
-        <!-- JAVASCRIPT -->
 
-        <?php include 'layouts/vendor-scripts.php'; ?>
+    <!-- Right Sidebar -->
+    <?php include 'layouts/right-sidebar.php'; ?>
+    <!-- /Right-bar -->
 
-        <script src="assets/libs/fullcalendar/index.global.min.js"></script>
+    <!-- JAVASCRIPT -->
 
-        <!-- Calendar init -->
-        <script src="assets/js/pages/calendar.init.js"></script>
+    <?php include 'layouts/vendor-scripts.php'; ?>
 
-        <!-- App js -->
-        <script src="assets/js/app.js"></script>
+    <script src="assets/libs/fullcalendar/index.global.min.js"></script>
+
+    <!-- Calendar init -->
+    <script src="assets/js/pages/calendar.init.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
 
     </body>
-</html>
+
+    </html>
