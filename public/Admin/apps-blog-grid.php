f9e5de3eb4a85f3dcf70fb26688d6480c9507058
AndreaAleAM <159818760+AndreaAleAM@users.noreply.github.com>
2024-03-26 22:22:43 -0600

    Contacto

--- public/Admin/apps-blog-grid.php
+++ public/Admin/apps-blog-grid.php
@@ -41,57 +41,104 @@
                 <!-- end page title -->
 
                 <!-- Contacto-->
-                <div class="row align-items-center"
-                    style="border: 1px solid #86ADFF; border-radius: 5px;  padding: 12px; margin-bottom: 20px;">
-                    <style>
-                        /* Estilos CSS para el título */
-                        .titulo {
-                            text-align: center;
-                            /* Centrar el texto */
-                            color: #333;
-                            /* Color llamativo (en este caso, naranja) */
-                            font-size: 24px;
-                            /* Tamaño del texto */
-                            font-weight: bold;
-                            /* Negrita */
-                            font-family: Arial, sans-serif;
-                            /* Fuente del texto */
-                        }
-                    </style>
-                    <div class="titulo">
-                    Contacto
-                    </div>
-                    <div style="border-top: 1px solid #86ADFF; padding-top: 10px;">
-                        <p style="font-size: 16px; line-height: 1.5; color: #666;">Estamos trabajando diligentemente
-                            para brindarte una experiencia única y enriquecedora. Mantente al tanto, ¡pronto estará
-                            lista para ti!
+
+                <div
+                    style="text-align: center; margin-top: 20px; background-color: #f9fafb; border-radius: 20px; padding: 30px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+                    <h1 style="font-size: 36px; font-family: 'Arial', sans-serif; color: #333; margin-bottom: 20px;">
+                        ¿Necesitas ayuda? ¡Estamos aquí para ti!
+                    </h1>
+                    <div style="border-top: 1px solid #ccc; padding-top: 20px; margin-top: 20px;">
+
+                        <p style="font-size: 20px; font-family: 'Arial', sans-serif; color: #666; margin-bottom: 30px;">
+                            Tu satisfacción es nuestra prioridad número uno. No dudes en contactarnos para cualquier
+                            consulta o asistencia que necesites.
                         </p>
+                        <div style="text-align: center; display: flex; flex-wrap: wrap; justify-content: center;">
+                            <div style="margin-right: 40px;">
+                                <a href="tel:7713306407" style="text-decoration: none; color: #333;">
+                                    <img src="assets/images/contacto/phone.png" alt="Icono de teléfono"
+                                        style="width: 50px; height: 50px; margin-bottom: 10px;">
+                                    <br>
+                                    <span style="font-size: 22px;">771 330 64 07</span>
+                                </a>
+                            </div>
+                            <div style="margin-right: 40px;">
+                                <a href="mailto:contacto@datalpine.mx" style="text-decoration: none; color: #333;">
+                                    <img src="assets/images/contacto/gmail.png" alt="Icono de correo"
+                                        style="width: 50px; height: 50px; margin-bottom: 10px;">
+                                    <br>
+                                    <span style="font-size: 22px;">contacto@datalpine.mx</span>
+                                </a>
+                            </div>
+                            <div style="margin-right: 40px;">
+                                <a href="https://datalpine.mx/" style="text-decoration: none; color: #333;">
+                                    <img src="assets/images/contacto/web.png" alt="Icono de sitio web"
+                                        style="width: 50px; height: 50px; margin-bottom: 10px;">
+                                    <br>
+                                    <span style="font-size: 22px;">Visita nuestro sitio web</span>
+                                </a>
+                            </div>
+                            <div style="margin-right: 40px;">
+                                <div>
+                                    <img src="assets/images/contacto/map.png" alt="Icono de ubicación"
+                                        style="width: 50px; height: 50px; margin-bottom: 10px;">
+                                    <br>
+                                    <span style="font-size: 22px;">Estamos en Pachuca de Soto, Hidalgo.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="margin-top: 30px;">
+                            <a href="https://www.facebook.com/profile.php?id=61552173002247"
+                                style="text-decoration: none; color: #333; margin-right: 20px;">
+                                <img src="assets/images/contacto/facebook.png" alt="Icono de Facebook"
+                                    style="width: 40px; height: 40px;">
+                            </a>
+                            <a href="https://twitter.com/DatAlpine"
+                                style="text-decoration: none; color: #333; margin-right: 20px;">
+                                <img src="assets/images/contacto/twitter.png" alt="Icono de Twitter"
+                                    style="width: 40px; height: 40px;">
+                            </a>
+                            <a href="https://www.instagram.com/DatAlpine/"
+                                style="text-decoration: none; color: #333; margin-right: 20px;">
+                                <img src="assets/images/contacto/instagram.png" alt="Icono de Instagram"
+                                    style="width: 40px; height: 40px;">
+                            </a>
+                            <a href="https://www.tiktok.com/@datalpine?_t=8l1AW0atban&_r=1"
+                                style="text-decoration: none; color: #333;">
+                                <img src="assets/images/contacto/tiktok.png" alt="Icono de TikTok"
+                                    style="width: 40px; height: 40px;">
+                            </a>
+                        </div>
+
                     </div>
+
+
+
+
                 </div> <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
 
-            </div> <!-- container-fluid -->
+            <?php include 'layouts/footer.php'; ?>
         </div>
-        <!-- End Page-content -->
+        <!-- end main content-->
 
-        <?php include 'layouts/footer.php'; ?>
     </div>
-    <!-- end main content-->
-
-</div>
-<!-- END layout-wrapper -->
+    <!-- END layout-wrapper -->
 
 
-<!-- Right Sidebar -->
-<?php include 'layouts/right-sidebar.php'; ?>
-<!-- /Right-bar -->
+    <!-- Right Sidebar -->
+    <?php include 'layouts/right-sidebar.php'; ?>
+    <!-- /Right-bar -->
 
-<!-- JAVASCRIPT -->
+    <!-- JAVASCRIPT -->
 
-<?php include 'layouts/vendor-scripts.php'; ?>
+    <?php include 'layouts/vendor-scripts.php'; ?>
 
-<!-- App js -->
-<script src="assets/js/app.js"></script>
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
