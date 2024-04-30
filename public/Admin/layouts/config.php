bae3af3e4355d783ac67c55b61ece499843a1178
MiguelDWT4toCreativo <158526754+MiguelDWT4toCreativo@users.noreply.github.com>
2024-04-30 11:41:25 -0600

    Update config.php
    No me muevan la BBDD por Favor, No Tocar!!!!
    
    Ya se ve de nuevo! :D

--- public/Admin/layouts/config.php
+++ public/Admin/layouts/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'datalpine');
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'tocreati_Adgabo');
+define('DB_PASSWORD', 'Qj+uxmwXC-H_');
+define('DB_NAME', 'tocreati_Prueba_dash');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
