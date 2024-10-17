4ec33680ce11d2354f4c700f634436c60e1edc66
Yoali Ambrosio <somerhalder96c@gmail.com>
2024-10-17 12:08:04 -0600

    Factibilidad Pachuca

--- public/Admin/pa-factibilidad.php
+++ public/Admin/pa-factibilidad.php
@@ -457,7 +457,7 @@
 
         <!-- Segmentación de mercado: Promedio de precio por segmento -->
         <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
     <div class="card">
         <div class="card-body">
             <h5 class="card-title subtitle mb-3 text-center">Segmentación de mercado: Distribución de propiedades por segmento</h5>
@@ -654,7 +654,7 @@
 </div>
 
 
-        <!-- Porcentaje de vivienda ofertadas por antigüedad -->
+        <!-- Porcentaje de vivienda ofertadas por antigüedad 
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
@@ -674,7 +674,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>-->
 
 <!-- Mediana del precio de oferta -->
 <div class="col-md-12">
@@ -982,7 +982,7 @@ El siguiente segmento, con créditos de menos de <strong>2 millones</strong> de 
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title subtitle mb-3 text-center">Mapa de Calor: Distribución de Propiedades</h5>
+                            <h5 class="card-title subtitle mb-3 text-center">Distribución de Propiedades</h5>
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <div class="d-flex align-items-center">
                                     <i class="fas fa-user-tie me-2"></i>
